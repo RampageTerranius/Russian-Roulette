ae939f46532bf47ec9cdc6c519b1ba60d85235d4
RampageTerranius <rampage_terranius@hotmail.com>
2024-11-06 21:55:30 +1100

    Add TODO comment

---
+++ Russian Roulette/Item.h
@@ -0,0 +1,65 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include "player.h"
+#include "gun.h"
+
+#include <xhash>// TODO: figure out WHY i need xhash here? am i using a custom hash somewhere that im not aware of?
+
+// Pure virtual base class for all items.
+// Children must implement all functions.
+class Item
+{
+public:
+	// Use the item, return true if item was used, false if it was not.
+	virtual bool Use(Player* turnPlayer, Player* opponentPlayer, Gun* gun) = 0;
+	virtual string ItemExplaination() = 0;
+	virtual string ItemName() = 0;
+};
+
+// Item that doubles the damage for the next shot.
+class DoubleDamage : public Item
+{
+public:
+	bool Use(Player* turnPlayer, Player* opponentPlayer, Gun* gun);
+	string ItemExplaination() { return "Saw off the barrel: Double the damage from the gun for current turn"; };
+	string ItemName() { return "Hack Saw"; };
+};
+
+// Item that gives the user an extra life.
+class GainLife : public Item
+{
+public:
+	bool Use(Player* turnPlayer, Player* opponentPlayer, Gun* gun);
+	string ItemExplaination() { return "Smoke cigarette: Gain one HP"; };
+	string ItemName() { return "Smokes"; };
+};
+
+// Item that allows the user to check if the next shell is live.
+class InspectShell : public Item
+{
+public:
+	bool Use(Player* turnPlayer, Player* opponentPlayer, Gun* gun);
+	string ItemExplaination() { return "Inspect gun: check if the current round in the gun is live or blank"; };
+	string ItemName() { return "Magnifying Glass"; };
+};
+
+// Item that racks the gun, removing a shell from the gun.
+class RackGun : public Item
+{
+public:
+	bool Use(Player* turnPlayer, Player* opponentPlayer, Gun* gun);
+	string ItemExplaination() { return "Rack the gun: Removes current round from gun"; };
+	string ItemName() { return "Beer"; };
+};
+
+// Item that forces the opponenet to skip their turn.
+class SkipOpponenetTurn : public Item
+{
+public:
+	bool Use(Player* turnPlayer, Player* opponentPlayer, Gun* gun);
+	string ItemExplaination() { return "Hand cuff opponent: Your opponenet skips their next turn"; };
+	string ItemName() { return "Hand cuffs."; };
+};
+
+#endif
